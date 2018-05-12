4aed33e492dc633cae54172bf517d6f52b3da9f4
Jason Juhyun Kim <mtinet@hanmail.net>
2018-05-12 18:34:57 +0900

    Create side.py

---
+++ last/side.py
@@ -0,0 +1,73 @@
+# 사이드 인디케이터(새버전)  
+# run: green, flash: red, flow: blue*2 - green
+import radio
+from random import randint
+from microbit import *
+import neopixel
+
+# Create the "flash" animation frames. Can you work out how it's done?
+flash = [Image().invert()*(i/9) for i in range(9, -1, -1)]
+
+# The radio won't work unless it's switched on.
+radio.on()
+np = neopixel.NeoPixel(pin0, 4)
+
+for pixel_id in range(0, len(np)):
+    red = 0
+    green = 200
+    blue = 0
+
+    # Assign the current LED a random red, green and blue value between 0 and 60
+    np[pixel_id] = (red, green, blue)
+
+    # Display the current pixel data on the Neopixel strip
+    np.show()
+        
+# Event loop.
+while True:
+    incoming = radio.receive()
+    if incoming == 'run':
+        for pixel_id in range(0, len(np)):
+
+            np[pixel_id] = (0, 200, 0)
+    
+            # Display the current pixel data on the Neopixel strip
+            np.show()
+            
+    if incoming == 'flash':
+        for num in range(0, 200):
+            for pixel_id in range(0, len(np)):
+                np[pixel_id] = (num, 0, 0)
+    
+            np.show()  
+            
+    if incoming == 'flow':
+        for num in range(0, 200):
+            for pixel_id in range(0, len(np)):
+                np[pixel_id] = (0, num, 0)
+    
+            np.show()
+            
+    if incoming == 'stop':
+        for pixel_id in range(0, len(np)):
+
+            np[pixel_id] = (0, 200, 0)
+    
+            # Display the current pixel data on the Neopixel strip
+            np.show()            
+
+    if incoming == 'auth':
+        for pixel_id in range(0, len(np)):
+
+            np[pixel_id] = (0, 200, 0)
+    
+            # Display the current pixel data on the Neopixel strip
+            np.show()          
+            
+    if incoming == 'close':
+        for pixel_id in range(0, len(np)):
+
+            np[pixel_id] = (200, 0, 0)
+    
+            # Display the current pixel data on the Neopixel strip
+            np.show()
